7dfb249f317be2a7c03833c71f4f61b5287f3026
Pow3rwing <cherrat.hassaan@gmail.com>
2016-02-14 21:19:07 +0100

    clean up code + tests

---
+++ scorebowling/src/main/java/com/projet/apps/scorebowling/exception/BadScoreLineException.java
@@ -0,0 +1,12 @@
+package com.projet.apps.scorebowling.exception;
+
+public class BadScoreLineException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public BadScoreLineException(String msg) {
+	super(msg);
+    }
+}
